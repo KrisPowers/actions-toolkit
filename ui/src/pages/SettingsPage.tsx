d01f6a0357376f66df0afbbfe8b569e432046ae5
Kris Powers <85710701+KrisPowers@users.noreply.github.com>
2026-07-25 17:41:07 -0400

    refactor(settings): fold general settings into single page

--- ui/src/pages/SettingsPage.tsx
+++ ui/src/pages/SettingsPage.tsx
@@ -1,17 +1,17 @@
-import { Outlet } from "react-router-dom";
 import PageHeader from "../components/common/PageHeader";
-import InstanceSettingsSidebar from "../components/settings/InstanceSettingsSidebar";
+import GithubConnectionCard from "../components/settings/GithubConnectionCard";
+import RuntimeSettingsCard from "../components/settings/RuntimeSettingsCard";
+import BucketSettingsCard from "../components/settings/BucketSettingsCard";
 
 export default function SettingsPage() {
   return (
     <div className="max-w-6xl">
       <PageHeader title="Settings" />
 
-      <div className="mt-5 grid grid-cols-1 gap-6 md:grid-cols-[200px_1fr]">
-        <InstanceSettingsSidebar />
-        <div className="min-w-0">
-          <Outlet />
-        </div>
+      <div className="mt-5 grid grid-cols-1 gap-5 xl:grid-cols-2">
+        <GithubConnectionCard />
+        <RuntimeSettingsCard />
+        <BucketSettingsCard />
       </div>
     </div>
   );
